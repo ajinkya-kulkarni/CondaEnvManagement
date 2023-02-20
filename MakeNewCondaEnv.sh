6b771b2bbd0814ed4099e8909a84f422402af229
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2023-02-20 22:38:46 +0100

    Hopefully (final) changes

--- MakeNewCondaEnv.sh
+++ MakeNewCondaEnv.sh
@@ -7,33 +7,37 @@ source ~/.zshrc
 set -eu
 
 # Install anaconda-clean first
-conda install anaconda-clean
+conda install anaconda-clean --yes
 
 # Function to create and activate an environment
 create_environment() {
-# Extract environment name and requirements file from input arguments
-name="$1"
-requirements_file="$2"
+	# Extract environment name and requirements file from input arguments
+	name="$1"
+	requirements_file="$2"
 
-# Create and activate the environment
-echo "Creating and activating $name environment"
-conda create --name "$name" python=3.9 --yes
-conda activate "$name"
+	# Create and activate the environment
+	echo "Creating and activating $name environment"
+	conda create --name "$name" python=3.9 --yes
+	conda activate "$name"
 
-# Install jupyter notebook
-conda install -c anaconda jupyter
+	# Install jupyter notebook
+	conda install -c anaconda jupyter --yes
 
-# Install packages in the environment
-echo "Installing packages in $name environment"
-curl -s "https://raw.githubusercontent.com/ajinkya-kulkarni/CondaEnvManagement/main/$requirements_file" | xargs -n 1 pip install
+	if [ "$name" == "napari" ]; then
+    	pip install "napari[all]"
+	fi
 
-# Clean the environment
-echo "Cleaning $name environment"
-conda clean --all --yes
+	# Install packages in the environment
+	echo "Installing packages in $name environment"
+	curl -s "https://raw.githubusercontent.com/ajinkya-kulkarni/CondaEnvManagement/main/$requirements_file" | xargs -n 1 pip install
 
-# Deactivate the environment
-echo "Deactivating $name environment"
-conda deactivate
+	# Clean the environment
+	echo "Cleaning $name environment"
+	conda clean --all --yes
+
+	# Deactivate the environment
+	echo "Deactivating $name environment"
+	conda deactivate
 }
 
 # Create and activate the general environment
