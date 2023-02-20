20f1f693a20a6555f1ecb35c4063ff2f54520f64
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2023-02-20 20:30:47 +0100

    Minor changes

--- MakeNewCondaEnv.sh
+++ MakeNewCondaEnv.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Clear the screen
-clear
-
 # Initialize the shell for Conda
 conda init zsh
 source ~/.zshrc
@@ -11,26 +8,26 @@ set -eu
 
 # Function to create and activate an environment
 create_environment() {
-  # Extract environment name and requirements file from input arguments
-  name="$1"
-  requirements_file="$2"
+# Extract environment name and requirements file from input arguments
+name="$1"
+requirements_file="$2"
 
-  # Create and activate the environment
-  echo "Creating and activating $name environment"
-  conda create --name "$name" python=3.9 --yes
-  conda activate "$name"
+# Create and activate the environment
+echo "Creating and activating $name environment"
+conda create --name "$name" python=3.9 --yes
+conda activate "$name"
 
-  # Install packages in the environment
-  echo "Installing packages in $name environment"
-  curl -s "https://raw.githubusercontent.com/ajinkya-kulkarni/CondaEnvManagement/main/$requirements_file" | xargs -n 1 pip install -q
+# Install packages in the environment
+echo "Installing packages in $name environment"
+curl -s "https://raw.githubusercontent.com/ajinkya-kulkarni/CondaEnvManagement/main/$requirements_file" | xargs -n 1 pip install -q
 
-  # Clean the environment
-  echo "Cleaning $name environment"
-  conda clean --all --yes
+# Clean the environment
+echo "Cleaning $name environment"
+conda clean --all --yes
 
-  # Deactivate the environment
-  echo "Deactivating $name environment"
-  conda deactivate
+# Deactivate the environment
+echo "Deactivating $name environment"
+conda deactivate
 }
 
 # Create and activate the general environment
@@ -42,3 +39,5 @@ create_environment "deeplearning" "requirements_deeplearning.txt"
 # Create and activate the napari environment
 create_environment "napari" "requirements_deeplearning.txt"
 pip install -q "napari[all]"
+
+echo "All done!"
