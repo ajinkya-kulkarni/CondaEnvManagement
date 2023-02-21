3732273bf15818ca928f1187c31b5cd2423be635
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2023-02-21 11:07:10 +0100

    Used `wget` instead of `curl`

--- MakeNewCondaEnv_MACOS.sh
+++ MakeNewCondaEnv_MACOS.sh
@@ -10,7 +10,6 @@ set -eu
 # Install anaconda-clean first
 if ! conda install anaconda-clean --yes; then
 	echo "Error: Failed to install anaconda-clean. Please check your internet connection or try again later."
-	exit 1
 fi
 
 # Function to create and activate an environment
@@ -22,24 +21,20 @@ create_environment() {
 	echo "Creating and activating $name environment"
 	if ! conda create --name "$name" python=3.9 --yes; then
 		echo "Error: Failed to create $name environment."
-		exit 1
 	fi
 	if ! conda activate "$name"; then
 		echo "Error: Failed to activate $name environment."
-		exit 1
 	fi
 
 	# Install jupyter notebook
 	if ! conda install -c anaconda jupyter --yes; then
 		echo "Error: Failed to install jupyter notebook in $name environment."
-		exit 1
 	fi
 
 	# Install packages in the environment
 	echo "Installing packages in $name environment"
-	if ! curl -s "https://raw.githubusercontent.com/ajinkya-kulkarni/CondaEnvManagement/main/requirements_common.txt" | xargs -n 1 pip install; then
+	if ! wget -qO- "https://raw.githubusercontent.com/ajinkya-kulkarni/CondaEnvManagement/main/requirements_common.txt" | xargs -n 1 pip install; then
 		echo "Error: Failed to install packages in $name environment."
-		exit 1
 	fi
 
 	if [ "$name" == "deeplearning" ]; then
@@ -64,14 +59,12 @@ create_environment() {
 	echo "Cleaning $name environment"
 	if ! conda clean --all --yes; then
 		echo "Error: Failed to clean $name environment."
-		exit 1
 	fi
 
 	# Deactivate the environment
 	echo "Deactivating $name environment"
 	if ! conda deactivate; then
 		echo "Error: Failed to deactivate $name environment."
-		exit 1
 	fi
 }
 
