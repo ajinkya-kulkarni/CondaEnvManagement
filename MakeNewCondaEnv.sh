d3c76f4806ded5438dda6bcc003d82d0211c4f02
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2023-02-21 08:11:08 +0100

    Updated `MakeNewCondaEnv.sh` with checks

--- MakeNewCondaEnv.sh
+++ MakeNewCondaEnv.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-# Initialize the shell for Conda
-conda init zsh
-source ~/.zshrc
+# Clear the output
+clear
+
+# Detect the operating system and initialize the appropriate shell
+if [[ $(uname -s) == 'Darwin' ]]; then
+	conda init zsh
+	source ~/.zshrc
+else
+	conda init bash
+	source ~/.bashrc
+fi
 
 set -eu
 
+# Check if Conda is installed
+if [ -x "$(command -v conda)" ]; then
+	echo "Error: Conda is not installed or the environment is not set up. Please install Conda and set up the environment."
+	exit 1
+fi
+
 # Install anaconda-clean first
-conda install anaconda-clean --yes
+if ! conda install anaconda-clean --yes; then
+	echo "Error: Failed to install anaconda-clean. Please check your internet connection or try again later."
+	exit 1
+fi
 
 # Function to create and activate an environment
 create_environment() {
@@ -17,29 +34,59 @@ create_environment() {
 
 	# Create and activate the environment
 	echo "Creating and activating $name environment"
-	conda create --name "$name" python=3.9 --yes
-	conda activate "$name"
+	if ! conda create --name "$name" python=3.9 --yes; then
+		echo "Error: Failed to create $name environment."
+		exit 1
+	fi
+	if ! conda activate "$name"; then
+		echo "Error: Failed to activate $name environment."
+		exit 1
+	fi
 
 	# Install jupyter notebook
-	conda install -c anaconda jupyter --yes
+	if ! conda install -c anaconda jupyter --yes; then
+		echo "Error: Failed to install jupyter notebook in $name environment."
+		exit 1
+	fi
 
 	# Install packages in the environment
 	echo "Installing packages in $name environment"
-	curl -s "https://raw.githubusercontent.com/ajinkya-kulkarni/CondaEnvManagement/main/$requirements_file" | xargs -n 1 pip install
+	if ! curl -s "https://raw.githubusercontent.com/ajinkya-kulkarni/CondaEnvManagement/main/$requirements_file" | xargs -n 1 pip install; then
+		echo "Error: Failed to install packages in $name environment."
+		exit 1
+	fi
 
 	if [ "$name" == "napari" ]; then
-		pip install pyqtwebengine==5.15
-    	pip install "napari[all]"
+		if ! pip install pyqtwebengine==5.15; then
+			echo "Error: Failed to install pyqtwebengine in $name environment."
+			exit 1
+		fi
+		if ! pip install "napari[all]"; then
+			echo "Error: Failed to install napari[all] in $name environment."
+			exit 1
+		fi
 	fi
 
 	# Clean the environment
 	echo "Cleaning $name environment"
-	conda clean --all --yes
+	if ! conda clean --all --yes; then
+		echo "Error: Failed to clean $name environment."
+		exit 1
+	fi
 
 	# Deactivate the environment
 	echo "Deactivating $name environment"
-	conda deactivate
+	if ! conda deactivate; then
+		echo "Error: Failed to deactivate $name environment."
+		exit 1
+	fi
 }
+
+# Check if the requirement files exist
+if [ ! -f requirements_common.txt ] || [ ! -f requirements_deeplearning.txt ]; then
+	echo "Error: One or more of the package requirement files are missing. Please make sure all required files are present."
+	exit 1
+fi
 
 # Create and activate the general environment
 create_environment "general" "requirements_common.txt"
@@ -51,6 +98,7 @@ create_environment "deeplearning" "requirements_deeplearning.txt"
 create_environment "napari" "requirements_deeplearning.txt"
 pip install "napari[all]"
 
+# Echo success message
 echo ""
 echo "All done!"
 echo ""
