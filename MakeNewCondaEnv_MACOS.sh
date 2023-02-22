dc4644cf6c9281ea88f389237d10a96d6f20e50c
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2023-02-22 08:50:39 +0100

    Final version fro MacOS

--- MakeNewCondaEnv_MACOS.sh
+++ MakeNewCondaEnv_MACOS.sh
@@ -6,19 +6,31 @@ set -euo pipefail
 # Clear the terminal output
 clear
 
+echo ""
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' _
+echo ""
+
 # Remove all environments except `base`
 for i in $(conda env list | awk '{print $1}' | egrep -v 'base|#' | tr '\n' ' '); do
-    if [ $i != "base" ]; then
-        echo "Removing environment $i"
-        conda env remove --name $i --yes
-    fi
+	if [ $i != "base" ]; then
+		echo "Removing environment $i"
+		conda env remove --name $i --yes
+	fi
 done
+
+echo ""
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' _
+echo ""
 
 # Define a function to create environments and install packages
 function create_environment 
 {
 	# Get the environment name from the first argument
 	name=$1
+
+	echo ""
+	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' _
+	echo ""
 
 	# Create the environment
 	echo "Creating environment $name"
@@ -27,6 +39,10 @@ function create_environment
 		exit 1
 	fi
 
+	echo ""
+	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' _
+	echo ""
+
 	# Activate the environment
 	echo "Activating $name environment"
 	if ! conda activate $name; then
@@ -34,36 +50,29 @@ function create_environment
 		exit 1
 	fi
 
+	echo ""
+	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' _
+	echo ""
+
 	# Install packages
 	echo "Installing packages in $name environment"
-	pip install numpy==1.24.0
-	pip install opencv-python-headless==4.7.0.68
-	pip install imageio==2.25.1
-	pip install scikit-image==0.19.3
-	pip install scipy==1.10.1
-	pip install matplotlib==3.7.0
-	pip install tqdm==4.64.1
-	pip install tifffile==2023.2.3
-	pip install Pillow==9.4.0
-	pip install streamlit==1.8.1
+	python -m pip install numpy==1.24.0 opencv-python-headless==4.7.0.68 imageio==2.25.1 scikit-image==0.19.3 scipy==1.10.1 matplotlib==3.7.0 tqdm==4.64.1 tifffile==2023.2.3 Pillow==9.4.0 streamlit==1.8.1
 
 	if [ "$name" == "deeplearning" ]; then
-		pip install tensorflow-cpu==2.11.0
-		pip install scikit-learn==1.2.1
-		pip install stardist==0.8.3
+		python -m pip install tensorflow-cpu==2.11.0 scikit-learn==1.2.1 stardist==0.8.3
 	fi
 
 	if [ "$name" == "napari" ]; then
-		pip install pyqtwebengine==5.15.6
-		pip install "napari[all]"
+		python -m pip install pyqtwebengine==5.15.6 "napari[all]"
 	fi
 
 	if [ "$name" == "ABAproject" ]; then
-		pip install boto3==1.17.107
-		pip install botocore==1.20.107
-		pip install caosdb==0.11.0
-		pip install caosadvancedtools==0.6.1
+		python -m pip install boto3==1.17.107 botocore==1.20.107 caosdb==0.11.0 caosadvancedtools==0.6.1
 	fi
+
+	echo ""
+	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' _
+	echo ""
 
 	# Clean the environment
 	echo "Cleaning $name environment"
@@ -71,9 +80,14 @@ function create_environment
 		echo "Error: Failed to clean $name environment."
 	fi
 
+	echo ""
+	printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' _
+	echo ""
+
 	# Deactivate the environment
 	echo "Deactivating $name environment"
 	conda deactivate
+
 }
 
 # Initialize conda and source zshrc
@@ -91,4 +105,5 @@ done
 # Echo success message
 echo ""
 echo "All done!"
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' _
 echo ""
